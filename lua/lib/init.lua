ee7e5f529b0c1e0c1dbfc31ff6f89a6a050bbaa2
aimixsaka <aimixsaka@gmail.com>
2024-01-27 14:03:44 +0800

    feat(lib): add `files_from_dir` and `modules_from_dir` function

--- lua/lib/init.lua
+++ lua/lib/init.lua
@@ -30,6 +30,20 @@ function lib.merge_list(lst1, lst2)
   end
   return lst1
 end
+
+function lib.files_from_dir(dir)
+  return io.popen('find ' .. dir .. ' -mindepth 1 -maxdepth 1 -type f'):lines()
+end
+
+function lib.modules_from_dir(dir)
+  local modules = {}
+  for file in lib.files_from_dir(dir) do
+    -- `gsub` see https://www.lua.org/pil/20.2.html
+    local module = file:gsub('%.lua', ''):match(lib.nvim_lua_dir .. '(.*)')
+    table.insert(modules, module)
+  end
+  return modules
+end
 -- body end
 
 return lib
