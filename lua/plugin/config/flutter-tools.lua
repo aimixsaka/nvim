6280e92db0ca097b18c2a12b9f4b3f0f2a0866c1
aimixsaka <aimixsaka@gmail.com>
2024-02-17 15:16:41 +0800

    fix(plugin): `require(module)` instead of `ipairs(require(module))`

---
+++ lua/plugin/config/flutter-tools.lua
@@ -0,0 +1,5 @@
+local lib = require('lib')
+
+lib.prequire('flutter-tools').setup({
+
+})
--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -20,10 +20,7 @@ local plugins = {}
 local plugin_path = lib.script_path()
 -- strip self to prevent loop require
 for _, module in ipairs(lib.modules_from_dir(plugin_path .. 'install/')) do
-  -- `gsub` see https://www.lua.org/pil/20.2.html
-  for _, plugin in ipairs(require(module)) do
-    table.insert(plugins, plugin)
-  end
+  table.insert(plugins, require(module))
 end
 
 local opts = {
